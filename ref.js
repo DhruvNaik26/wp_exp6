5d84209a4dca6e92c5aeb417faba919ae46afbb6
DhruvNaik26 <148272817+DhruvNaik26@users.noreply.github.com>
2023-11-01 04:47:24 -0700

    Update ref.js

--- ref.js
+++ ref.js
@@ -1,53 +1,53 @@
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('.button button');
 
-    const display = document.getElementById("display");
-    const buttons = document.querySelectorAll(".button button");
+let currentInput = "";
+let currentOp = "";
+let prevInput = "";
 
-    let currentInput = "";
-    let currentOperator = "";
-    let previousInput = "";
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const value = button.textContent;
 
-    buttons.forEach(button => {
-        button.addEventListener("click", () => {
-            const value = button.textContent;
-
-            if (value >= '0' && value <= '9') {
-                currentInput += value;
-                display.textContent = currentInput;
-            } else if (value === "=") {
-                if (currentOperator && previousInput !== "") {
-                    currentInput = operate(currentOperator, parseFloat(previousInput), parseFloat(currentInput));
-                    display.textContent = currentInput;
-                    previousInput = "";
-                    currentOperator = "";
-                }
-            } else {
-                if (currentOperator) {
-                    if (previousInput !== "") {
-                        currentInput = operate(currentOperator, parseFloat(previousInput), parseFloat(currentInput));
-                        display.textContent = currentInput;
-                        previousInput = currentInput;
-                        currentInput = "";
-                    }
-                }
-                currentOperator = value;
-                previousInput = currentInput;
-                currentInput = "";
-            }
-        });
-    });
-
-    function operate(operator, num1, num2) {
-        switch (operator) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '*':
-                return num1 * num2;
-            case '/':
-                return num1 / num2;
-            default:
-                return num2;
+        if (value >= '0' && value <= '9' || value === '+' || value === '-' || value === '*' || value === '/') {
+            currentInput += value;
+            display.textContent = currentInput;
         }
-    }
 
+        if (value === 'C') {
+            display.textContent = '0';
+            currentOp = "";
+            currentInput = "";
+            prevInput = "";
+        } else if (value === '=') {
+            if (currentOp && currentInput !== "") {
+                currentInput = cal(currentOp, parseFloat(prevInput), parseFloat(currentInput));
+                display.textContent = currentInput;
+                prevInput = "";
+                currentOp = "";
+            }
+        } else if (value === '+' || value === '-' || value === '*' || value === '/') {
+            currentOp = value;
+            prevInput = currentInput;
+            // currentInput = "";
+        }
+    });
+});
+
+function cal(op, num1, num2) {
+    switch (op) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 === 0) {
+                return "Error"; // Handle division by zero
+            }
+            return num1 / num2;
+        default:
+            return "Error"; // Handle invalid operator
+    }
+}
